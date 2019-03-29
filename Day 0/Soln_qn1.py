83c1d15c772b3e3e323c7a31cc4b8804af90a29d
sruthipraveenkumargeetha <sruthi17pg@gmail.com>
2019-03-29 20:15:06 +0530

    Day 0 qn 1

---
+++ Day 0/Soln_qn1.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter
+from itertools import groupby
+import statistics
+def main():
+    dimension=int(input())
+    x=[int(i) for i in input().split()]
+    print(statistics.mean(x))
+    print(statistics.median(x))
+    freqs=groupby(Counter(x).most_common(), lambda x:x[1])
+    print(min([val for val,count in next(freqs)[1]]))
+    #print(statistics.mode(x))
+if __name__ =="__main__":
+    main()
+
